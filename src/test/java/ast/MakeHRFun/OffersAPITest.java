b3d35462cb17bf7c3b4b0094c1bc21542b396e04
Aneta Stępień <aneta.j.stepien@gmail.com>
2019-03-03 11:25:45 +0100

    Test

---
+++ src/test/java/ast/MakeHRFun/OffersAPITest.java
@@ -0,0 +1,32 @@
+package ast.MakeHRFun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class OffersAPITest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testOfferList_empty() throws Exception {
+        assertThat(this.restTemplate
+                .getForObject("http://localhost:" + port + "/offers", String.class))
+                .contains("[]");
+    }
+
+}
